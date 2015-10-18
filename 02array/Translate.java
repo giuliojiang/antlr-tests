c4513cce262c1f36c62fdfffd451ba43aacc6145
Giulio Jiang <giuliojiang@gmail.com>
2015-10-18 18:25:53 +0100

    added translate program

--- 02array/Translate.java
+++ 02array/Translate.java
@@ -1,5 +1,20 @@
+import org.antlr.v4.runtime.ANTLRInputStream;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.ParseTree;
+import org.antlr.v4.runtime.tree.ParseTreeWalker;
 
 public class Translate
 {
-    
+    public static void main(String[] args) throws Exception
+    {
+        ANTLRInputStream input = new ANTLRInputStream(System.in);
+        ArrayInitLexer lexer = new ArrayInitLexer(input);
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+        ArrayInitParser parser = new ArrayInitParser(tokens);
+        ParseTree tree = parser.init();
+        
+        ParseTreeWalker walker = new ParseTreeWalker();
+        walker.walk(new ConverterListener(), tree);
+        System.out.println();
+    }
 }
