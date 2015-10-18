0a2664fd6af0ea31fa27107b6bc9603ded568139
Giulio Jiang <giuliojiang@gmail.com>
2015-10-18 18:05:03 +0100

    simple parser for array initializers from "the definitive antlr 4 reference"

---
+++ 02array/ArrayInit.g4
@@ -0,0 +1,11 @@
+/* Grammar header */
+grammar ArrayInit;
+
+init : '{' value ( ',' value )* '}' ;
+
+value : init
+      | INT
+      ;
+
+INT : [0-9]+ ; 
+WS  : [ \t\r\n]+ -> skip ;
---
+++ 02array/Test.java
@@ -0,0 +1,29 @@
+import org.antlr.v4.runtime.ANTLRInputStream;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.ParseTree;
+
+public class Test
+{
+
+    public static void main(String[] args) throws Exception
+    {
+        // create CharStream
+        ANTLRInputStream input = new ANTLRInputStream(System.in);
+        
+        // create lexer
+        ArrayInitLexer lexer = new ArrayInitLexer(input);
+        
+        // create tokens buffer
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+        
+        // create parser
+        ArrayInitParser parser = new ArrayInitParser(tokens);
+        
+        // make the tree
+        ParseTree tree = parser.init();
+        
+        // print tree
+        System.out.println(tree.toStringTree(parser));
+    }
+    
+}
