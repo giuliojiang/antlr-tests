265290e87fe2905911aa9edfddf6a10ab3169208
Giulio  Jiang <giulio.jiang14@imperial.ac.uk>
2015-10-19 15:35:06 +0100

    added Expr

---
+++ 03arith/ExprJoyRide.java
@@ -0,0 +1,32 @@
+import java.io.FileInputStream;
+import java.io.InputStream;
+
+import org.antlr.v4.runtime.ANTLRInputStream;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.ParseTree;
+
+
+public class ExprJoyRide {
+
+    public static void main(String[] args) throws Exception
+    {
+	String inputFile = null;
+	if (args.length > 0)
+	{
+	    inputFile = args[0];
+	}
+	InputStream is = System.in;
+	if (inputFile != null)
+	{
+	    is = new FileInputStream(inputFile);
+	}
+	
+	ANTLRInputStream input = new ANTLRInputStream(is);
+	ExprLexer lexer = new ExprLexer(input);
+	CommonTokenStream tokens = new CommonTokenStream(lexer);
+	ExprParser parser = new ExprParser(tokens);
+	ParseTree tree = parser.prog();
+	System.out.println(tree.toStringTree(parser));
+    }
+    
+}
